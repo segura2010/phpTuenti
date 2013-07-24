f2904eb41973e35317f2ca8f1ec531eeb8c754ae
alberto__segura <alberto.segura.delgado@gmail.com>
2013-07-24 12:02:30 +0200

    Update examples.php

--- examples.php
+++ examples.php
@@ -8,7 +8,7 @@ header('Content-Type: text/html; charset=UTF-8');
 $miTuenti = new Tuenti('miemail@dominio.com', 'mipasssword');
 $r = $miTuenti->getFriendsData();
 
-
+//Obtenemos los amigos con numero de telefono
 foreach($r as $f)
   if($f->phone_number != '')
 		echo "<br>".$f->name." ".$f->surname." Phone: ".$f->phone_number;
