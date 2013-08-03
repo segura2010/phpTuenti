73375d88f56b806355c72c46a4df56306a28ae00
alberto__segura <alberto.segura.delgado@gmail.com>
2013-08-03 14:23:45 +0200

    Update examples.php

--- examples.php
+++ examples.php
@@ -5,13 +5,22 @@ require_once("Tuenti.php");
 header('Content-Type: text/html; charset=UTF-8');
 
 //Test
-$miTuenti = new Tuenti('miemail@dominio.com', 'mipasssword');
-$r = $miTuenti->getFriendsData();
+$miTuenti = new Tuenti('email@email.com', 'password');
 
-//Obtenemos los amigos con numero de telefono
-foreach($r as $f)
-  if($f->phone_number != '')
-		echo "<br>".$f->name." ".$f->surname." Phone: ".$f->phone_number;
+
+if($miTuenti->login()) //Primero conectamos para obtener una sesion!
+{
+	$r = $miTuenti->getFriendsData();
+
+	//Obtenemos los amigos con numero de telefono
+	foreach($r as $f)
+  		if($f->phone_number != '')
+			echo "<br>".$f->name." ".$f->surname." Phone: ".$f->phone_number;
+
+}
+else
+	echo "Hubo un error en al conectar!";
+	
 
 
 ?>
